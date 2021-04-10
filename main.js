b246987d1037b96810b1bbdbe08bdacdf6beb95d
Verkstad-projekt <82270045+Verkstad-projekt@users.noreply.github.com>
2021-04-10 14:25:51 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function formClick(){
+    document.getElementById("sendbtn").innerHTML="<p>Ditt svar har skickats.</p>"
+}
+
+function back2top(){
+    document.body.scrollTop=0;
+    document.documentElement.scrollTop=0;
+}
